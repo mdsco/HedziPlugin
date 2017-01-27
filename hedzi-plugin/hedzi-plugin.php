55aad481ff3fa88dc4a9d311dc86348b5f3f35a2
mike <scoboria@gmail.com>
2017-01-27 17:05:36 -0500

    Changed dashboard info in hedzi-plugin.php

--- hedzi-plugin/hedzi-plugin.php
+++ hedzi-plugin/hedzi-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Hedzi Plugin
 Plugin URI:
-Description: Hedzi Fancy Product Designer Mod
+Description: Hedzi Fancy Product Designer Layout Manager Mod
 Author: Mike Scoboria
 Author URI:
 Version: 0.1
@@ -10,19 +10,19 @@ Version: 0.1
 
 add_action("admin_menu", "addMenu");
 
-function addMenu(){
-	add_menu_page("Example Options", "Example Options", 4, "example_options", "exampleMenu");
-}
+// function addMenu(){
+// 	add_menu_page("Example Options", "Example Options", 4, "example_options", "exampleMenu");
+// }
 
-function exampleMenu(){
-	echo "hello world";
-}
+// function exampleMenu(){
+// 	echo "hello world";
+// }
 
 function twentysixteen_child_scripts(){
 
-    wp_register_script( 'extra js', plugins_url( '/js/extras2.js', __FILE__ ), array('jquery') );
+    wp_register_script( 'Layout Manager Mods', plugins_url( '/js/layout-manager-mod.js', __FILE__ ), array('jquery') );
 
-	wp_enqueue_script('extra js');
+	wp_enqueue_script('Layout Manager Mods');
 }
 
 add_action('wp_enqueue_scripts', 'twentysixteen_child_scripts');
